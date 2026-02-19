36eb86e991080573bf5e2b8db54ce87cfd359e13
Amr Zakaria <amr3zakarya@gmail.com>
2026-02-19 17:41:08 +0200

    fix resizable pannel

--- Frontend/src/layouts/mainLayout/MainLayout.tsx
+++ Frontend/src/layouts/mainLayout/MainLayout.tsx
@@ -2,53 +2,65 @@ import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from "@/components/ui/resizable"
-import { Outlet } from "react-router-dom"
-import LeftSidebar from "./LeftSidebar"
-import Topbar from "@/components/Topbar"
+} from "@/components/ui/resizable";
+import { Outlet } from "react-router-dom";
+import LeftSidebar from "./LeftSidebar";
+import Topbar from "@/components/Topbar";
 import { useEffect, useState } from "react";
 
 const MainLayout = () => {
-	const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-	useEffect(() => {
-		const checkMobile = () => {
-			setIsMobile(window.innerWidth < 768);
-		};
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
 
-		checkMobile();
-		window.addEventListener("resize", checkMobile);
-		return () => window.removeEventListener("resize", checkMobile);
-	}, []);
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
 
-	return (
-		<div className='h-screen bg-black text-white flex flex-col'>
-			<ResizablePanelGroup direction='horizontal' className='flex-1 flex h-full overflow-hidden p-2'>
-				
-				{/* left sidebar */}
-				<ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
-					<LeftSidebar />
-				</ResizablePanel>
+  return (
+    <div className="h-screen bg-black text-white flex flex-col">
+      <Topbar />
+      <ResizablePanelGroup
+        orientation="horizontal"
+        className="flex-1 flex h-full overflow-hidden p-2"
+      >
+        {/* left sidebar */}
+        <ResizablePanel
+          defaultSize="20%"
+          minSize={isMobile ? "0%" : "10%"}
+          maxSize="30%"
+        >
+          <LeftSidebar />
+        </ResizablePanel>
 
-				<ResizableHandle className='w-2 bg-black rounded-lg transition-colors' />
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
 
-				{/* Main content */}
-				<ResizablePanel defaultSize={isMobile ? 80 : 60}>
-					<Outlet />
-				</ResizablePanel>
+        {/* Main content */}
+        <ResizablePanel defaultSize={isMobile ? "80%" : "60%"}>
+          <Outlet />
+        </ResizablePanel>
 
-				{!isMobile && (
-					<>
-						<ResizableHandle className='w-2 bg-black rounded-lg transition-colors' />
+        {!isMobile && (
+          <>
+            <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
 
-						{/* right sidebar */}
-						<ResizablePanel defaultSize={20} minSize={0} maxSize={25} collapsedSize={0}>
-							right side bar
-						</ResizablePanel>
-					</>
-				)}
-			</ResizablePanelGroup>
-		</div>
-	);
+            {/* right sidebar */}
+            <ResizablePanel
+              defaultSize="20%"
+              minSize="0%"
+              maxSize="25%"
+              collapsedSize="0%"
+            >
+              right side bar
+            </ResizablePanel>
+          </>
+        )}
+      </ResizablePanelGroup>
+    </div>
+  );
 };
 export default MainLayout;
